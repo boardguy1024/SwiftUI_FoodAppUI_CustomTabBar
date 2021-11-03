c058f17df524d51812598c0727da030be0068229
boardguy1024 <boardguy1024@gmail.com>
2021-11-04 00:19:53 +0900

    first commit

---
+++ SwiftUI_FoodAppUI_CustomTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_FoodAppUI_CustomTabBar
+//
+//  Created by park kyung seok on 2021/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_FoodAppUI_CustomTabBar/SwiftUI_FoodAppUI_CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FoodAppUI_CustomTabBarApp.swift
+//  SwiftUI_FoodAppUI_CustomTabBar
+//
+//  Created by park kyung seok on 2021/11/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FoodAppUI_CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
